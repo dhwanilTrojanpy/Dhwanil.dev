48532725f2ac6c6390aba8109ce72a8a13fa76ae
Dhwanil Gandhi <60458476+dhwanilTrojanpy@users.noreply.github.com>
2025-03-14 11:13:17 -0400

    add all files

---
+++ src/constants/index.js
@@ -0,0 +1,305 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Dhruv Gangwani',
+    position: 'Data Scientist at Outlier',
+    img: 'assets/review1.png',
+    review:
+      'I enthusiastically recommend Dhwanil, who excelled as an AI Trainer-Software Engineer at Outlier. His innovative LLM training pipelines reduced model training time by 20% while boosting accuracy. Dhwanil integrated LangChain for real-time monitoring and resolved critical data drift issues swiftly. A collaborative problem-solver, he automated QA processes, cutting post-deployment bugs by 30%. A visionary in AI/software engineering.',
+      email: 'dhruvgangwani44@gmail.com',
+
+    },
+  {
+    id: 2,
+    name: 'Michael Regan',
+    position: 'Founder at Leader Frames',
+    img: 'assets/review2.png',
+    review:
+      'Dhwanil delivered exceptional work as a software developer at Leader Frames, building critical features and maintaining internal tools with precision. His ability to consistently complete projects ahead of deadlines while ensuring flawless functionality made him indispensable. A proactive problem-solver with sharp technical skills—any team would benefit from his talent and reliability.                          ',
+      email: 'mregan@leaderframes.com',
+
+    },
+  {
+    id: 3,
+    name: 'Sravan Kumar', 
+    position: 'HR and Director at ITSM America ',
+    img: 'assets/review3.png',
+    review:
+      'Dhwanil excelled as a Full Stack Engineer, delivering scalable FastAPI/.NET solutions that accelerated financial data processing by 40%. His React.js optimizations cut load times by 30%, while GenAI integrations boosted predictive analytics accuracy. A strategic problem-solver who consistently exceeded deadlines and elevated cross-functional collaboration—an invaluable asset to any tech team.',
+    email: 'hr@itsmamerica.com',
+    },
+  {
+    id: 4,
+    name: 'Jay Shah',
+    position: 'Senior Flutter Developer at Goldenmace', 
+    img: 'assets/review4.png',
+    review:
+      'I had the pleasure of collaborating with Dhwanil at Goldenmace, where he showcased exceptional skill as a Python Developer. His work on Raspberry Pi APIs and Flask deployments streamlined IoT solutions, while his secure JWT/OAuth implementations and MQTT integrations ensured robust system communication. A resourceful engineer who delivers innovative, production-ready code under tight deadlines. Highly recommended!',
+      email: 'dhwanilgandhi28@gmail.com',
+
+    },
+];
+
+export const myProjects = [
+  {
+    title: 'Podcastr - AI Podcast Platform',
+    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'LiveDoc - Real-Time Google Docs Clone',
+    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+    subdesc:
+      'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
+    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'CarePulse - Health Management System',
+    desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+    subdesc:
+      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
+    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Horizon - Online Banking Platform',
+    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    subdesc:
+      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Imaginify - AI Photo Manipulation App',
+    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+    subdesc:
+      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+    {
+      "id": 1,
+      "name": "Reality AI Labs",
+      "pos": "Generative AI Engineer",
+      "duration": "2025 - Present",
+      "title": "Built AI education tools using LangChain/OpenAI LLMs (GPT-4, Llama-3) and Python(FastAPI) with hybrid RAG architectures, improving semantic search precision by 25% and reducing inference costs via model quantization.",
+      "icon": "/assets/realityai.jpeg",
+      "animation": "victory"
+    },
+    {
+      "id": 2,
+      "name": "ITSM America",
+      "pos": "Software Engineer",
+      "duration": "2022 - Present",
+      "title": "Developed Python/React/C# microservices (FastAPI/.NET) for insurance/finance platforms, achieving 70% faster claims processing and 99.9% uptime at 50k+ RPM using cloud-native architectures.",
+      "icon": "/assets/genai.jpeg",
+      "animation": "clapping"
+    },
+    {
+      "id": 3,
+      "name": "NRI Distributions",
+      "pos": "Software Engineer",
+      "duration": "2022",
+      "title": "Engineered Python/Flask APIs with PostgreSQL/MongoDB, reducing API latency by 40% through query optimization and AWS serverless deployment (Lambda/EC2).",
+      "icon": "/assets/nri.jpeg",
+      "animation": "salute"
+    },
+    {
+      "id": 4,
+      "name": "Goldenmace IT",
+      "pos": "Python Developer",
+      "duration": "2019-2021",
+      "title": "Built real-time IoT systems using Python/Flask with MQTT and Azure, achieving 98% uptime for distributed Raspberry Pi networks through async programming.",
+      "icon": "/assets/goldenmace.jpeg",
+      "animation": "waving"
+    }
+  
+];

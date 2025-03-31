87c36290cf3efae8acfdb5bd956f633b60e9bd29
Dhwanil Gandhi <60458476+dhwanilTrojanpy@users.noreply.github.com>
2025-03-31 19:02:41 +0000

    changes for button and links

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -56,8 +56,8 @@ const Hero = () => {
       </div>
 
       <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
-        <a href="#about" className="w-fit">
-          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+        <a href="#contact" className="w-fit">
+          <Button name="Learn About Me" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
         </a>
       </div>
     </section>
